34ecc15c768919a242a2063eacdf2e517f5e9b2e
Stuart Williams <stuartw@magenic.com>
2018-07-20 11:53:58 -0500

    Initial Project

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # bWappLab #
+Security Lab Training
+
+you can also use the docker container: 
+
+* https://hub.docker.com/r/raesene/bwapp/ 
+* https://hub.docker.com/r/feltsecure/owasp-bwapp/
---
+++ docker-run.cmd
@@ -0,0 +1,6 @@
+docker run -d -p 9002:80 feltsecure/owasp-bwapp
+@echo start http://localhost:9002 to browse to the site, 
+@echo if not installed, browse to http://localhost:9002/install.php
+
+
+ 
